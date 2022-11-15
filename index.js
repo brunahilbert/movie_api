e05ce93597151e0622d6bc46d25eb4f6b7d8b99b
brunahilbert <bruna.hilbertt@gmail.com>
2022-11-15 16:55:43 +0100

    updated index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express'),
   morgan = require('morgan'),
   fs = require('fs'),
+  path = require('path');
+
 const app = express();
 
 // create a write stream (in append mode)
@@ -73,3 +75,29 @@ let topMovies = [
 // setup the logger
 app.use(morgan('combined', { stream: accessLogStream }));
 
+// routes all requests for static files to the 'public' folder
+app.use(express.static('public'));
+
+// GET requests
+app.get('/', (req, res) => {
+  res.send('Welcome to your Flix!');
+});
+
+app.get('/documentation', (req, res) => {
+  res.sendFile('public/documentation.html', { root: __dirname });
+});
+
+app.get('/movies', (req, res) => {
+  res.json(topMovies);
+});
+
+// error-handling middleware function
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+// listen for requests
+app.listen(8080, () => {
+  console.log('Your app is listening on port 8080.');
+});
